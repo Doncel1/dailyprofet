6c9a15278da43a92f50dfca5c841c6cc452c30a5
Doncel1 <sergio.romerodoncel@hotmail.com>
2022-03-30 16:31:00 -0500

    rio

---
+++ HappyLand_dailyprofet/Assets/_Scripts/CuracionRio.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuracionRio : MonoBehaviour
+{
+    VidaPlayer playerVida;
+
+    public int cantidad;
+    public float damageTime;
+    float currentDamageTime;
+
+    void Start()
+    {
+        playerVida = GameObject.FindWithTag("Player").GetComponent<VidaPlayer>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            currentDamageTime += Time.deltaTime;
+            if (currentDamageTime > damageTime)
+            {
+                playerVida.vida += cantidad;
+                currentDamageTime = 0.0f;
+                
+
+            }
+        }
+    }
+
+
+}
