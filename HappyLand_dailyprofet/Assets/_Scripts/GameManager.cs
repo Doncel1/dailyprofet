85065954c8c823c4651d49c9d79c85f8f785a67e
mm201-19 <mm201-19@k401-6.local>
2022-05-27 10:33:53 -0500

    Paneles de ganar y perder

--- HappyLand_dailyprofet/Assets/_Scripts/GameManager.cs
+++ HappyLand_dailyprofet/Assets/_Scripts/GameManager.cs
@@ -6,6 +6,12 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public bool gameOver = false;
+    [SerializeField]
+    private int _runestowin = 0;
+
+    public bool winner = false;
+    public GameObject screenWinner;
+    public GameObject screenLoser;
 
     public void Awake()
     {
@@ -17,5 +23,46 @@ public class GameManager : MonoBehaviour
           gameOver = true;  
     }
 
+    public void playerwinner (int ObjectToWin) 
+    {
+        if(gameOver==false)
+        {
+            if(_runestowin<=3)
+            {
+                _runestowin = _runestowin + ObjectToWin;
+                    if(_runestowin==3)
+                {
+                    winner=true;
+                    InterfaceManager();
+                    gameOver = true;    
 
+                }
+            }
+        }
+    }
+    public void PlayerLose(bool gameStatus)
+    {
+        if (gameOver==false && gameStatus==true)
+        {
+            gameOver = gameStatus;
+            InterfaceManager(); 
+        }
+     
+    }
+
+    public void InterfaceManager()
+    {
+        if(winner==true) 
+        {
+            screenWinner.SetActive(true);
+        }
+        if (gameOver==true)
+        {
+            screenLoser.SetActive(true);
+        }
+    }
 }
+
+
+
+
