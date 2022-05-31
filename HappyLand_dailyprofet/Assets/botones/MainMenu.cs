a617623a9b76e873d77aa9bb27cb5393d3a4e853
Doncel1 <99496329+Doncel1@users.noreply.github.com>
2022-05-31 00:34:50 -0500

    Pantalla inicio

--- HappyLand_dailyprofet/Assets/botones/MainMenu.cs
+++ HappyLand_dailyprofet/Assets/botones/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -15,4 +16,11 @@ public class MainMenu : MonoBehaviour
     {
         
     }
+
+    public void EscenaJuego()
+    {
+        SceneManager.LoadScene("Forest_Scene");
+          
+    }
+
 }
